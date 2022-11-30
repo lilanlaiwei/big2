6bef4358218dcfd8fd3bd42a6e1c32e3816ce75b
Riran Seo <lilanlaiwei@gmail.com>
2022-11-30 14:20:00 +0900

    Add a command

--- sample2.c
+++ sample2.c
@@ -5,41 +5,50 @@
 
 void countAllChr(unsigned int count[], char *str) {
 
-    for (unsigned int i = 0; str[i] != '\0'; i++) {
-            count[(unsigned char)str[i]]++;
-    }
+	for (unsigned int i = 0; str[i] != '\0'; i++) {
+	    	count[(unsigned char)str[i]]++;   
+	}
 }
 
-int main(int argc) {
+int main(void) {
 	    
-    FILE *fr;
-    char t1[] = "ko.txt";
-    char str[MAX_LINE];
-    unsigned int count[CHR_NUM];
+	FILE *fr;
+	char t1[] = "ko.txt";
+    	char str[MAX_LINE];
+	char pi[CHR_NUM];
+    	unsigned int count[CHR_NUM];
+	int m = 0;
+	int moji;
 
-			    
-    fr = fopen(t1, "r");
-    if (fr == NULL) {
-	    printf("%s file open error!!\n", t1);
-	    return -1;
-    }
 
-    for (unsigned int i = 0; i < CHR_NUM; i++) {
-	   if(i >= 'a' && i <= 'z')
-		count[i] = 0;
-    }
+	fr = fopen (t1,"r");
+	if (fr == NULL){
+		printf("%s file open error!!\n", t1);
+		return -1;
+	}
 
-    while (fgets(str, MAX_LINE, fr) != NULL) {
-	    countAllChr(count, str);
-    }
+    	for (unsigned int i = 0; i < CHR_NUM; i++) {
+     		if(i >= 'a' && i <= 'z')
+			count[i] = 0;
+    	}
 
-    printf("in [%s]\n", t1);
-    for (unsigned int i = 0; i < CHR_NUM; i++) {
-	    if(i >= 'a' && i <= 'z')
-	    	printf("%c : %u\n", i, count[i]);
-    }
 
-    fclose(fr);
+	while (fgets(str, MAX_LINE, fr) != NULL) {
+    		countAllChr(count, str);
+	}
 
-    return 0;
+
+	printf("in [%s]\n", t1);
+      	for (unsigned int i = 0; i < CHR_NUM; i++) {
+		if(i >= 'a' && i <= 'z'){
+			printf("%c : %u\n", i, count[i]);
+			m += count[i];
+		}
+	}
+
+	printf("文字数：[%d]\n", m); 
+    	
+	fclose(fr);
+
+	return 0;
 }
